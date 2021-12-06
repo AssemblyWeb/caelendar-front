138e9a66490d771750d737a8afd4ec0215cd2bd7
Alfio Perino <alfio@assemblyweb.net>
2021-12-06 16:57:24 -0300

    first commit

---
+++ precache-manifest.e5fda7a77f52a935c5ee3b91830cab01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a06be7c16732b0e2fb448d5db573b84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2be79f90bcc8d65d7506",
+    "url": "/static/css/2.59a0ab78.chunk.css"
+  },
+  {
+    "revision": "9b55d8d337be438d7cac",
+    "url": "/static/css/main.6020f20c.chunk.css"
+  },
+  {
+    "revision": "2be79f90bcc8d65d7506",
+    "url": "/static/js/2.bd64b146.chunk.js"
+  },
+  {
+    "revision": "fa4b4056bba234820139274405351863",
+    "url": "/static/js/2.bd64b146.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b55d8d337be438d7cac",
+    "url": "/static/js/main.f627256a.chunk.js"
+  },
+  {
+    "revision": "c2ab9c45931d577cf16e",
+    "url": "/static/js/runtime-main.27b01e2e.js"
+  }
+]);
